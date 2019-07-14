68f7533b7bf46ef473e42d4119a5201915c242ce
Trong <ductrongtran.tdt@gmail.com>
2019-07-14 20:37:18 +0700

    Initial commit

---
+++ DatXanhManagement/DatXanhManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatXanhManagement
+//
+//  Created by Trong Tran on 7/14/19.
+//  Copyright © 2019 Trong Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
